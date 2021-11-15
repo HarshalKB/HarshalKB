4e5a70962acfecf03e1054de6fe39f5441a62126
Harshal Bombarde <harshalbombarde@gmail.com>
2021-11-15 18:16:09 +0530

    Create Greatest_Number.java

---
+++ Greatest_Number.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.CloseAction;
+
+public class Greatest_Number{
+    public static void main(String arhs[]){
+        Scanner sc = new Scanner(System.in);
+        int num_1 = sc.nextInt();
+        int num_2 = sc.nextInt();
+        int num_3 = sc.nextInt();
+        if (num_1 > num_2 && num_1 > num_3){
+            System.out.print(num_1);
+        } else if (num_2 > num_1 && num_2 > num_3){
+            System.out.print(num_2);
+        } else {
+            System.out.print(num_3);
+        }
+    }
+}
